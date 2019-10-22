e1443a1c33d7baefc35f38d64fb7c4f0319c1f0e
Minibrams <anders@brams.dk>
2019-10-22 14:08:07 +0200

    Prediction results sent to server

--- collector/src/app/components/predictions/predictions.component.ts
+++ collector/src/app/components/predictions/predictions.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Entity } from 'src/app/models/entity';
+import { SentimentResult } from 'src/app/models/sentiment-result';
+import { EntitySentiment } from 'src/app/models/entity-sentiment';
 
 @Component({
   selector: 'app-predictions',
@@ -9,6 +11,9 @@ import { Entity } from 'src/app/models/entity';
 export class PredictionsComponent implements OnInit {
   @Input() negPredictions: Entity[];
   @Input() posPredictions: Entity[];
+  @Output() result = new EventEmitter<SentimentResult>();
+
+  private sentimentResult = new SentimentResult;
 
   constructor() { }
 
@@ -16,4 +21,21 @@ export class PredictionsComponent implements OnInit {
     
   }
 
+  sentiment(event: EntitySentiment) {
+    let entities = this.posPredictions.some(o => event.entity == o) ? this.posPredictions : this.negPredictions;
+
+    entities.splice(entities.indexOf(event.entity), 1);
+
+    if (event.sentiment === 1) {
+      this.sentimentResult.liked.push(event.entity.uri);
+    } else if (event.sentiment === -1) {
+      this.sentimentResult.disliked.push(event.entity.uri);
+    } else {
+      this.sentimentResult.unknown.push(event.entity.uri);
+    }
+
+    if (!entities.length) {
+      this.result.emit(this.sentimentResult);
+    }
+  }
 }
